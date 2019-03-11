3b9b5c75e25ec1dd6d61f4c74eaa52ebb01dc0cd
pmatthews05 <mrpaulmatthews@live.co.uk>
2019-03-11 09:03:09 +0000

    Ensure WelcomePage is skipped

--- ExportClientSidePages.ps1
+++ ExportClientSidePages.ps1
@@ -3,7 +3,11 @@ $destinationUrl = "https://tenant2.sharepoint.com/sites/test"
 
 #Connect to the sharepoint environment
 Connect-PnPOnline -url $sourceUrl -UseWebLogin
-$WebTitle = (Get-PnPWeb).Title
+$Web = Get-PnPWeb
+$Context = Get-PnPContext
+$Context.Load($Web)
+Invoke-PnPQuery
+
 # Define global constants
 $pnp = [System.Xml.Linq.XNamespace]"http://schemas.dev.office.com/PnP/2018/07/ProvisioningSchema"
 
@@ -19,6 +23,13 @@ $listItems = Get-PnpListItem -list "Site Pages"
 #Add every single page to the target template
 foreach ($items in $listItems) {
     if ($listItems.FieldValues["ContentTypeId"].StringValue.StartsWith("0x0101009D1CB255DA76424F860D91F20E6C4118")) {
+        
+        if($Web.WelcomePage.EndsWith($items.FieldValues["FileLeafRef"])
+        {
+            #Welcome page already included from getting ProvisioningTemplate PageContents.
+            continue;
+        }
+        
         $pageXml = [xml](Export-PnPClientSidePage -Identity $items.FieldValues["FileLeafRef"])
         $pageXElement = [System.Xml.Linq.XElement]::Parse($pageXml.Provisioning.Templates.ProvisioningTemplate.ClientSidePages.ClientSidePage.OuterXml)
         $clientSidePages.Add($pageXElement)
